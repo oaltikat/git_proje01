454a2b7fbc6d556c8745378ca8e48508d21b59ff
oaltikat <oaltikat@gmail.com>
2020-06-21 15:33:19 -0400

    feature branch'ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		System.out.println("Ilk commit from local repository");
 		System.out.println("Ikinci commit from local repository");
 		
+		System.out.println("Yeni branch olusturduk feature adinda");
 
 	}
 
